0dabd0b7366ea19d28144e7f4ea6c059c97bd324
Maciej Paluch <maciekpal@gmail.com>
2022-11-15 20:04:20 +0100

    Changed path to: "/refresh-products"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ProductsListComponent } from './components/products-list/products-list.component';
-import { ProductsListComponentModule } from './components/products-list/products-list.component-module';
-import { ProductsServiceModule } from './services/products.service-module';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ProductsListComponent} from './components/products-list/products-list.component';
+import {ProductsListComponentModule} from './components/products-list/products-list.component-module';
+import {ProductsServiceModule} from './services/products.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products', component: ProductsListComponent }]), ProductsListComponentModule, ProductsServiceModule],
+  imports: [RouterModule.forRoot([{
+    path: 'refresh-products',
+    component: ProductsListComponent
+  }]), ProductsListComponentModule, ProductsServiceModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
